8bc8b2a0ecc86b344c54966aca80832474367103
SangNguyen <nngocsang38@gmail.com>
2024-10-24 10:23:03 +0700

    add horizontal poster

--- MovieApp.Application/Feature/Movie/Dtos/MovieInfoLanding.cs
+++ MovieApp.Application/Feature/Movie/Dtos/MovieInfoLanding.cs
@@ -9,6 +9,7 @@ public class MovieInfoLanding
     public int? NumberOfRatings { get; set; }
     public int? SumOfRatings { get; set; }
     public string Poster { get; set; }
+    public string HorizontalPoster { get; set; }
     public string Slug { get; set; }
     public int AgeRestriction { get; set; }
     public List<FormatResponse> Formats { get; set; } = new();
